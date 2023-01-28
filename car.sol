57c2443c0e92f2d053e27b2d83a673afa4ea0a37
iamxumy <123818755+iamxumy@users.noreply.github.com>
2023-01-29 03:21:00 +0800

    Update car.sol

--- car.sol
+++ car.sol
@@ -4,9 +4,10 @@ pragma solidity >=0.6.0 <=0.8.0;
  import "./Token.sol";
  
 contract CarRental is Ownable {
+   
     string public name = "Car Rental";
     Token token; //Reference to deployed ERC20 Token contract
-    address payable wallet; //Address of the owner of Car Rental Shop    
+    address public payable wallet; //Address of the owner of Car Rental Shop
     uint8 collateralPremium=20; // discount % to be applied to standard rate for PREMIUM "collateralized customers"
     uint8 tokenConversionRate = 2; //conversion rate between Ether and Token, i.e. 1 Ether = 2 Token
     uint rate=1; //amount of wei to be charged per second (i.e. "standard rate")
@@ -14,11 +15,17 @@ contract CarRental is Ownable {
     uint tokenMinBalance= 1; //minimum amount of Tokens required to start Car rental   
     uint etherCollateralThreshold=1; //Threshold amount of ETH required to get PREMIUM "collateralized" rate
     uint tokenCollateralThreshold=1; //Threshold amount of Tokens required to get PREMIUM "collateralized" rate
-
-
+    uint public no_of_agreement = 0; //agreement no.
+    
     struct Car {                
+        uint8 CarId; // Id of the car
+        string carbrand;  // characteristcs of the car
+        string color;
+        string type;
+        uint rent_per_hour;
+        uint securityDeposit;
         bool notAvailable;
-        address customer;              
+        address customer; 
     }
 
     struct Customer { 
@@ -34,6 +41,11 @@ contract CarRental is Ownable {
     mapping (address => Customer) customers ; // Record with customers data (i.e., balance, startTie, debt, rate, etc)
     mapping (uint8 => Car) Cars ; // Stock of Cars    
 
+    modifier onlyCompany() {
+        require(msg.sender == wallet, "Only company can access this");
+        _;
+    }
+   
     modifier OnlyWhileNoPending(){
         require(customers[msg.sender].etherDebt == 0, "Not allowed to rent if debt is pending");
         _;
@@ -45,12 +57,17 @@ contract CarRental is Ownable {
     }
 
     modifier OnlyOneRental(){
-        require(!customers[msg.sender].isRenting, "Another bike rental in progress. Finish current rental first");
+        require(!customers[msg.sender].isRenting, "Another car rental in progress. Finish current rental first");
         _;
     }
 
     modifier EnoughRentFee(){
         require(customers[msg.sender].etherBalance >= etherMinBalance || customers[msg.sender].tokenBalance >= tokenMinBalance, "Not enough funds in your account");
+        _;
+    }
+    
+    modifie sameCustomer(uint8 _CarId) {
+        require(msg.sender == Cars[_CarId].customer, "No previous agreement found with you & company");
         _;
     }
 
